7723db60468fe4ffc76208c0405436fb9fc0033a
dtonon <github@dtonon.com>
2025-05-14 16:11:57 +0200

    Config custom colors and text sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontSize: {
+				sm: ['16px', '18px'],
+				base: ['18px', '20px'],
+				lg: ['20px', '22px'],
+				xl: ['22px', '24px']
+			},
+			colors: {
+				accent: '#e32a6d'
+			},
+			lineHeight: {
+				normal: '0.8'
+			}
+		}
+	},
+	plugins: []
+};
